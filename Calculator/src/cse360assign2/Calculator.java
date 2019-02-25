fb410671a975c4b5f686ea5373cf7541e22b1b56
Dekart K. <dekartkosa1997@gmail.com>
2019-02-25 11:31:30 -0700

    Version 1 of the calculator

---
+++ Calculator/src/cse360assign2/Calculator.java
@@ -0,0 +1,44 @@
+/* CSE 360 Assignment 2
+ * Name: Dekart Kosa
+ * Class ID: 445
+ */
+
+package cse360assign2;
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	// This method takes the parameter and adds it to the total
+	public void add (int value) {
+		
+	}
+	
+	// This method takes the parameter and subtracts it from the total
+	public void subtract (int value) {
+		
+	}
+	
+	// This method takes the parameter and multiplies it to the total
+	public void multiply (int value) {
+		
+	}
+	
+	// This method takes the total and divides by the parameter
+	public void divide (int value) {
+		
+	}
+	
+	// This method gets the history of the calculator
+	public String getHistory () {
+		return "";
+	}
+
+}
