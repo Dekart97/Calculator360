1c74cf539be28884530e5924a025db98a5e8a9c1
Dekart K. <dekartkosa1997@gmail.com>
2019-02-25 13:22:03 -0700

    Version 3. Finished the getHistory method.

--- Calculator/src/cse360assign2/Calculator.java
+++ Calculator/src/cse360assign2/Calculator.java
@@ -7,6 +7,12 @@ package cse360assign2;
 public class Calculator {
 
 	private int total;
+	private String addString;
+	private String subString;
+	private String multString;
+	private String divString;
+	private String printString;
+	private String historyString = "0";
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
@@ -19,22 +25,30 @@ public class Calculator {
 	// This method takes the parameter and adds it to the total
 	public void add (int value) {
 		total = total + value;
+		addString = " + " + value;
+		historyString = historyString + addString;
 	}
 	
 	// This method takes the parameter and subtracts it from the total
 	public void subtract (int value) {
-		total = value - total;
+		total = total - value;
+		subString = " - " + value;
+		historyString = historyString + subString;
 	}
 	
-	// This method takes the parameter and multiplies it to the total
+	// This method takes the parameter and multiplies it by total
 	public void multiply (int value) {
 		total = value*total;
+		multString = " * " + value;
+		historyString = historyString + multString;
 	}
 	
 	// This method takes the total and divides by the parameter
 	public void divide (int value) {
 		if (value != 0){
             total = total/value;
+            divString = " / " + value;
+            historyString = historyString + divString; 
 		}else{
 		    total = 0;
 		}
@@ -42,7 +56,8 @@ public class Calculator {
 	
 	// This method gets the history of the calculator
 	public String getHistory () {
-		return "";
+		System.out.println(historyString);
+		return historyString;
 	}
 
 }
